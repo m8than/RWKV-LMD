779151825fbc510df50cc25285eee60042ec8c12
Nathan <me@nwilce.co.uk>
2024-05-02 17:45:14 +0100

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -10,7 +10,7 @@ from pytorch_lightning.utilities import rank_zero_info
 from .binidx import MMapIndexedDataset
 from .utils import MaybeIsPrime
 
-
+# Temporary fix dataset resuming by randomising dataset
 class MMapDataset(Dataset):
     def __init__(self, args):
         self.args = args
@@ -20,12 +20,20 @@ class MMapDataset(Dataset):
         self.ctx_len = args.ctx_len
         self.args.dataset_len = self.count
 
+        # for random sampling
+        global_rank = self.args.trainer.global_rank
+        current_time = int(time.time() * 1000)
+        seed_value = global_rank + current_time
+        np.random.seed(seed_value)
+
     def __len__(self):
         return self.count
-
+         
     def __getitem__(self, idx):
         if self.args.random_data:
             i = np.random.randint(0, self.data_size - (self.ctx_len+1))
+            print(i)
+            print(self.args.trainer.global_rank)
             data_chunk = self.data.get(idx=0, offset=i, length=self.ctx_len + 1).astype(int)
         else:
             data_chunk = self.data.get(idx=0, offset=idx * self.ctx_len, length=self.ctx_len + 1).astype(int)
